522cad67b28cfb371030567c5dcd5587e0dbf7c5
afadhilah <afadhilah2004@gmail.com>
2025-03-23 11:11:03 +0700

    Add user input handling and binary conversion for non-negative integers

--- 1.py
+++ 1.py
@@ -1,1 +1,9 @@
-asadasd
+# Baca input dari user
+N = int(input("Masukkan bilangan non-negatif: "))
+
+# Pastikan N adalah non-negatif
+if N < 0:
+    print("Input harus bilangan non-negatif.")
+else:
+    # Konversi ke biner dan cetak (tanpa prefix '0b')
+    print(bin(N)[2:])
