92b27cb08f9653e037132e0900a4ad744c4b2946
afadhilah <afadhilah2004@gmail.com>
2025-03-23 11:11:40 +0700

    Refactor binary conversion code and move to a new file

--- 1.py
+++
@@ -1,9 +0,0 @@
-# Baca input dari user
-N = int(input("Masukkan bilangan non-negatif: "))
-
-# Pastikan N adalah non-negatif
-if N < 0:
-    print("Input harus bilangan non-negatif.")
-else:
-    # Konversi ke biner dan cetak (tanpa prefix '0b')
-    print(bin(N)[2:])
---
+++ ConvertToBinary.py
@@ -0,0 +1,6 @@
+
+N = int(input())
+if N < 0:
+    print("")
+else:
+    print(bin(N)[2:])
